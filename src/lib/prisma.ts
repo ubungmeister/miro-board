ca1a89cd5c3ee659dec2e5f802dceff84dc6642c
ubungmeister <ubungmeister@gmail.com>
2025-04-06 17:45:01 +0200

    unused file deleted

--- src/lib/prisma.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-// Create a new PrismaClient instance
-const prismaClient = new PrismaClient()
-
-// Export the PrismaClient instance
-export const prisma = prismaClient 
